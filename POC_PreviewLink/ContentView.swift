72266c87bdc23ccd8683295de386447d4cafa26f
Theo Sementa <theosementa@gmail.com>
2025-01-07 20:29:52 +0100

    Initial Commit

---
+++ POC_PreviewLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POC_PreviewLink
+//
+//  Created by Theo Sementa on 07/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POC_PreviewLink/POC_PreviewLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_PreviewLinkApp.swift
+//  POC_PreviewLink
+//
+//  Created by Theo Sementa on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct POC_PreviewLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
